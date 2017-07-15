06a40e0631bf8b933f1a39bd410b41d2d5e7017f
souptikmajumder <soujd.2012@gmail.com>
2017-07-15 13:59:00 +0530

    Create pascal.py

---
+++ pascal.py
@@ -0,0 +1,16 @@
+
+
+for i in range(0,4):
+	for j in range(0,i+1):
+		print("1",end="")
+	print("")	
+
+sum=1
+for i in range(0,5):
+	for j in range(0,i+1):
+		sum=sum+j;
+		print(sum,end="")
+		sum=1
+	print("")	
+
+		
